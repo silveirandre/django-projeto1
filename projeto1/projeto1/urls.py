ac1aac137f023f8780a66972d8a67f4fd5442971
silveirandre <silveirandreac@gmail.com>
2022-10-27 12:34:48 -0500

    outro commit

--- projeto1/projeto1/urls.py
+++ projeto1/projeto1/urls.py
@@ -13,17 +13,13 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from http.client import HTTPResponse
+#from http.client import HTTPResponse
 
 from django.contrib import admin
-from django.urls import path
-
-
-def my_view(request):
-    return HTTPResponse('HOME')
-
+from django.http import HttpResponse
+from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', my_view),  # home
+    path('', include('recipes.urls'))
 ]
