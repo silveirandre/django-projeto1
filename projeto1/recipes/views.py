85c5fcf1b47e9e47fd4f6b89a68f29e277c4b3d9
silveirandre <silveirandreac@gmail.com>
2022-11-17 17:32:52 -0500

    removido contato e sobre

--- projeto1/recipes/views.py
+++ projeto1/recipes/views.py
@@ -1,14 +1,6 @@
-from django.http import HttpResponse
+#from django.http import HttpResponse
 from django.shortcuts import render
 
 
 def home(request):
     return render(request, 'home.html')
-
-
-def sobre(request):
-    return HttpResponse('SOBRE')
-
-
-def contato(request):
-    return HttpResponse('CONTATO')
